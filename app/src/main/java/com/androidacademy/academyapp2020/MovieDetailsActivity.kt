4c29816bead88cd16d6c350320ffe4975f5beece
bumis95 <bumis95@gmail.com>
2020-11-11 12:47:11 +0300

    Add MovieDetailsActivity

---
+++ app/src/main/java/com/androidacademy/academyapp2020/MovieDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.androidacademy.academyapp2020
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MovieDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+    }
+}
