e7aa63ea707f6b3d16e7e90831a1249b67692fcd
bumis95 <bumis95@gmail.com>
2020-11-28 11:16:54 +0300

    Add landscape orientation with 4 items in line instead 2

--- app/src/main/java/com/androidacademy/academyapp2020/view/ui/FragmentMoviesList.kt
+++ app/src/main/java/com/androidacademy/academyapp2020/view/ui/FragmentMoviesList.kt
@@ -1,5 +1,6 @@
 package com.androidacademy.academyapp2020.view.ui
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,9 +8,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.androidacademy.academyapp2020.R
 import com.androidacademy.academyapp2020.view.adapter.FilmAdapter
 import com.androidacademy.academyapp2020.view.adapter.ItemDecorator
-import com.androidacademy.academyapp2020.R
 
 class FragmentMoviesList : Fragment(), FilmAdapter.OnItemClickListener {
 
@@ -25,7 +26,14 @@ class FragmentMoviesList : Fragment(), FilmAdapter.OnItemClickListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         view.findViewById<RecyclerView>(R.id.recyclerView).apply {
-            layoutManager = GridLayoutManager(context, 2)
+            when (resources.configuration.orientation) {
+                Configuration.ORIENTATION_LANDSCAPE -> {
+                    layoutManager = GridLayoutManager(context, 4)
+                }
+                Configuration.ORIENTATION_PORTRAIT -> {
+                    layoutManager = GridLayoutManager(context, 2)
+                }
+            }
             adapter = filmAdapter
             addItemDecoration(ItemDecorator(left = 6, right = 6, bottom = 6, top = 6))
         }
