4cb8a9eeae52ab0868fc54a3ed6714d8cb0a34c9
bumis95 <bumis95@gmail.com>
2020-12-14 13:04:10 +0300

    Add Parcelize

--- app/src/main/java/com/androidacademy/academyapp2020/data/model/Actor.kt
+++ app/src/main/java/com/androidacademy/academyapp2020/data/model/Actor.kt
@@ -1,7 +1,11 @@
 package com.androidacademy.academyapp2020.data.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Actor(
     val id: Int,
     val name: String,
     val picture: String,
-)
+) : Parcelable
--- app/src/main/java/com/androidacademy/academyapp2020/data/model/Genre.kt
+++ app/src/main/java/com/androidacademy/academyapp2020/data/model/Genre.kt
@@ -1,6 +1,10 @@
 package com.androidacademy.academyapp2020.data.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Genre(
     val id: Int,
     val name: String
-    )
+) : Parcelable
