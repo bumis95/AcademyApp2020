4977865cf0be8c941f3ecad8f38c77826f74661b
bumis95 <bumis95@gmail.com>
2021-01-04 14:22:02 +0300

    Remove kotlin parcelize

--- app/src/main/java/com/androidacademy/academyapp2020/data/entity/Actor.kt
+++ app/src/main/java/com/androidacademy/academyapp2020/data/entity/Actor.kt
@@ -1,14 +1,11 @@
 package com.androidacademy.academyapp2020.data.entity
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-@Parcelize
 @Serializable
 data class Actor(
     @SerialName("id") val id: Int,
     @SerialName("name") val name: String,
     @SerialName("profile_path") val picture: String = "",
-) : Parcelable
+)
--- app/src/main/java/com/androidacademy/academyapp2020/data/entity/Genre.kt
+++ app/src/main/java/com/androidacademy/academyapp2020/data/entity/Genre.kt
@@ -1,13 +1,10 @@
 package com.androidacademy.academyapp2020.data.entity
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-@Parcelize
 @Serializable
 data class Genre(
     @SerialName("id") val id: Int,
     @SerialName("name") val name: String
-) : Parcelable
+)
